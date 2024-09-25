41bef314475af615d6443b7142795cc7543c9f06
Gustavo Teixeira <gustavo.teixeira2199@gmail.com>
2024-09-25 00:26:44 -0300

    Event update

--- src/main/java/com/devsuperior/demo/controllers/EventController.java
+++ src/main/java/com/devsuperior/demo/controllers/EventController.java
@@ -1,9 +1,10 @@
 package com.devsuperior.demo.controllers;
 
+import com.devsuperior.demo.dto.EventDTO;
 import com.devsuperior.demo.services.EventService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/events")
@@ -11,4 +12,9 @@ public class EventController {
 
     @Autowired
     private EventService service;
+
+    @PutMapping("/{id}")
+    public ResponseEntity<EventDTO> update(@PathVariable Long id, @RequestBody EventDTO dto) {
+        return ResponseEntity.ok(service.update(id, dto));
+    }
 }
--- src/main/java/com/devsuperior/demo/services/EventService.java
+++ src/main/java/com/devsuperior/demo/services/EventService.java
@@ -1,13 +1,33 @@
 package com.devsuperior.demo.services;
 
+import com.devsuperior.demo.dto.EventDTO;
+import com.devsuperior.demo.entities.City;
+import com.devsuperior.demo.entities.Event;
 import com.devsuperior.demo.repositories.EventRepository;
+import com.devsuperior.demo.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class EventService {
 
     @Autowired
     private EventRepository repository;
+
+    @Transactional
+    public EventDTO update(Long id, EventDTO dto) {
+        try {
+            Event entity = repository.getReferenceById(id);
+            entity.setName(dto.getName());
+            entity.setDate(dto.getDate());
+            entity.setUrl(dto.getUrl());
+            entity.setCity(new City(dto.getCityId(), null));
+            return new EventDTO(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException("Recurso não encontrado");
+        }
+    }
 
 }
