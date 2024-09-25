d406aaa2625f2ae17b25040aabea83bf935540b6
Gustavo Teixeira <gustavo.teixeira2199@gmail.com>
2024-09-24 23:26:00 -0300

    City findAllSortedByName

--- src/main/java/com/devsuperior/demo/controllers/CityController.java
+++ src/main/java/com/devsuperior/demo/controllers/CityController.java
@@ -1,9 +1,14 @@
 package com.devsuperior.demo.controllers;
 
+import com.devsuperior.demo.dto.CityDTO;
 import com.devsuperior.demo.services.CityService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/cities")
@@ -11,4 +16,9 @@ public class CityController {
 
     @Autowired
     private CityService service;
+
+    @GetMapping
+    public ResponseEntity<List<CityDTO>> findAllSortedByName() {
+        return ResponseEntity.ok(service.findAllSortedByName());
+    }
 }
--- src/main/java/com/devsuperior/demo/services/CityService.java
+++ src/main/java/com/devsuperior/demo/services/CityService.java
@@ -1,13 +1,27 @@
 package com.devsuperior.demo.services;
 
+import com.devsuperior.demo.dto.CityDTO;
+import com.devsuperior.demo.entities.City;
 import com.devsuperior.demo.repositories.CityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 public class CityService {
 
     @Autowired
     private CityRepository repository;
+
+    @Transactional(readOnly = true)
+    public List<CityDTO> findAllSortedByName() {
+        List<City> list = repository.findAll(Sort.by("name"));
+        return list.stream().map(CityDTO::new).toList();
+    }
+
+
 
 }
